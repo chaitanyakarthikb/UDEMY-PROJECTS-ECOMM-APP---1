fdd576448314ca1c40736ae7c2c47c2b578693de
Chaitanya karthik basva <chaitanyakarthikbasva@gmail.com>
2025-11-15 12:11:50 +0530

    implemented header functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world, I am learning react</h1>
+      <Header/>
      
     </div>
   );
