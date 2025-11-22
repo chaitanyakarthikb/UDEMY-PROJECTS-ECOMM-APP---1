1828ea2a763bd58fca7b244133b25b66157cebd5
Chaitanya karthik basva <chaitanyakarthikbasva@gmail.com>
2025-11-22 16:56:21 +0530

    implemented home section functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import FeaturedProducts from './Components/FeaturedProducts/FeaturedProducts';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
+      <FeaturedProducts/>
+
      
     </div>
   );
